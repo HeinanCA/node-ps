ba9d2ddfe39b041df8e62cfa9dabda80e82a0f37
Heinan Cabouly <heinancabouly@gmail.com>
2023-05-13 23:16:49 +0300

    Changed static port to environment variable

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const path = require('path');
 
+// Here goes environment variables
+const PORT = process.env.PORT || 3000;
+// Here ends environment variables
+
 const app = express();
 
 app.use(morgan('tiny'));
@@ -13,6 +17,6 @@ app.get('/', (req, res) => {
     res.send("Hello from Heinan Cabouly application!");
 })
 
-app.listen(3000, () => {
-    debug(`Hello! I'm listening to port: ${chalk.green('3000')}`);
+app.listen(PORT, () => {
+    debug(`Hello! I'm listening to port: ${chalk.green(PORT)}`);
 })
