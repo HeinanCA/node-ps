fccf37005541989dad9382211dbef326815c69e8
Heinan Cabouly <heinancabouly@gmail.com>
2023-05-15 23:51:09 +0300

    Added actual HTML code for website

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("Hello from Heinan Cabouly application!");
+    res.render('index', { title: "Heinan's website", data: ['a', 'b', 'c']});
 })
 
 app.listen(PORT, () => {
