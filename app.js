14591f815ddaf56e2aafde781196f398556a442e
Heinan Cabouly <heinancabouly@gmail.com>
2023-05-17 23:19:13 +0300

    Moved to work with index.ejs and proper sessions.ejs with EJS data

--- app.js
+++ app.js
@@ -9,12 +9,28 @@ const PORT = process.env.PORT || 3000;
 // Here ends environment variables
 
 const app = express();
+const sessionsRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+// TODO: Will move this out soon enough to a different function!
+sessionsRouter.route('/').get((req, res) => {
+    res.render('sessions', {sessions: [
+        { title: "Session 1", description: "This is going to be Session no. 1" },
+        { title: "Session 2", description: "This is going to be Session no. 2" },
+        { title: "Session 3", description: "This is going to be Session no. 3" },
+    ]});
+});
+
+sessionsRouter.route('/1').get((req, res) => {
+    res.send("Hello from a single session")
+});
+
+app.use('/sessions', sessionsRouter);
 
 app.get('/', (req, res) => {
     res.render('index', { title: "Heinan's website", data: ['a', 'b', 'c']});
